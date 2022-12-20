1e865cd8a7d9e82b9227d8dc298fb3a5e17f235a
Варвара <pti4ka12345@mail.ru>
2022-12-20 18:01:33 +0500

    created model user

--- core/models.py
+++ core/models.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractBaseUser):
-    first_name = models.CharField(max_length=200, null=True)
-    last_name = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=12, null=True)
-    email = models.EmailField(max_length=100, unique=True)
+class User(AbstractUser):
+    # first_name = models.CharField(max_length=200, null=True)
+    # last_name = models.CharField(max_length=200, null=True)
+    # phone = models.CharField(max_length=12, null=True)
+    # email = models.EmailField(max_length=100, unique=True)
+    pass
--- todolist/settings.py
+++ todolist/settings.py
@@ -10,20 +10,29 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-from os import environ
 from pathlib import Path
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+#
+# env = environ.Env(DEBUG=(bool, False), SECRET_KEY=(str, "spifq9otuo"))
+# # Quick-start development settings - unsuitable for production
+# # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+#
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = env('SECRET_KEY')
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = env('DEBUG')
+import os
+import environ
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-env = environ.Env(DEBUG=(bool, False), SECRET_KEY=(str, "spifq9otuo"))
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
@@ -77,17 +86,17 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
-}
+        }
 
 
-
+# DATABASES = {"default": env("DATABASE_URL")}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -118,7 +127,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+AUTH_USER_MODEL = "core.User"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
