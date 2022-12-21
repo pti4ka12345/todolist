b0b8a3f4e9c539194010d26f4b3acddbd4b1d286
Варвара <pti4ka12345@mail.ru>
2022-12-21 21:10:57 +0500

    создала суперюзера, зашла в админку

--- core/admin.py
+++ core/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from core.models import User
 
 
 @admin.register(User)
-class BoardAdmin(admin.ModelAdmin):
-    exclude = ("password",)
-    readonly_fields = ("date_joined", "last_login")
-    list_display = ("username", "email", "first_name", "last_name")
-    list_filter = ("is_staff", "is_active", "is_superuser")
-    search_fields = ("username", "email", "first_name", "last_name")
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (
+            'Personal Info',
+            {'fields': ('first_name', 'last_name', 'email')}
+        ),
+        (
+            'Permission',
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (
+            'Important dates',
+            {'fields': ('last_login', 'date_joined')}
+        )
+    )
+
+
+admin.site.unregister(Group)
